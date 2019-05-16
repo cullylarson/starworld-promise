df4044daee23664bf363993200b6977b16b9410a
Cully Larson <cully.larson@gmail.com>
2019-05-15 19:10:36 -0700

    Added a test to the then function to make sure it doesn't propagate rejected promises or cause them to be unhandled.

--- test/promise.then.test.js
+++ test/promise.then.test.js
@@ -1,8 +1,22 @@
 import {then} from '../esm/'
+import {pipe} from '@cullylarson/f'
 
 test('Works the same as a normal then.', () => {
-    expect.assertions(1)
-
     return then(x => x + 1, Promise.resolve(1))
         .then(x => expect(x).toBe(2))
+})
+
+test('Doesn\'t propagate rejected promises causing them to be unhandled.', () => {
+    expect.assertions(1)
+
+    const error = Error('blah')
+
+    return pipe(
+        then(x => {
+            throw error
+        }),
+        then(x => x + 1),
+        then(x => expect(x).toBe('Should never reach this line.'))
+    )(Promise.resolve(3))
+        .catch(err => expect(err).toBe(error))
 })
